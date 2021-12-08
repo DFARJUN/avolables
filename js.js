2bbf94149172b248ac04d9873f27e7aee3e5aba4
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-12-08 16:04:26 +0200

    d

--- js.js
+++ js.js
@@ -1,7 +1,10 @@
 
 document.onload = function addlocations() {
+  console.log("stage1")
 
     function readTextFile(file, callback) {
+      console.log("stage2")
+
         var rawFile = new XMLHttpRequest();
         rawFile.overrideMimeType("application/json");
         rawFile.open("GET", file, true);
@@ -15,6 +18,7 @@ document.onload = function addlocations() {
     
     //usage:
     readTextFile("locations.json", function(text){
+      console.log("stage3")
         var data = JSON.parse(text);
         console.log(data);
         quary = data;
@@ -22,12 +26,16 @@ document.onload = function addlocations() {
     });
   
   function myLoop(x) { 
+    console.log("stage4")
 
         var element = document.getElementById("demo");
 
         var i, y, xLen;
         xLen = Object.keys(quary).length;
         for (i = 0; i < xLen; i++) {
+          console.log("stage5")
+          console.log(quary[i].location_name+"("+quary[i].Zones__name+")")
+
           var newa = document.createElement("a");
           var locationname = quary[i].location_name+"("+quary[i].Zones__name+")";
           newa.innerHTML = locationname;
