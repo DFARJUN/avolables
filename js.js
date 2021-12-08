1b56a7b81a26e86210a8fe3b16940db53b8edcc6
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-12-08 16:02:47 +0200

    d

--- js.js
+++ js.js
@@ -1,8 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
 
 document.onload = function addlocations() {
 
