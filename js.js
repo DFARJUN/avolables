3a50a8fadf36579cb6d5d20d577206afedf284ac
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-12-08 16:40:42 +0200

    f

--- js.js
+++ js.js
@@ -1,45 +1,30 @@
-function addlocations() {
-  console.log("stage1")
+var location_nametxt
+var Zones__nametxt
 
-    function readTextFile(file, callback) {
-      console.log("stage2")
-
-        var rawFile = new XMLHttpRequest();
-        rawFile.overrideMimeType("application/json");
-        rawFile.open("GET", file, true);
-        rawFile.onreadystatechange = function() {
-            if (rawFile.readyState === 4 && rawFile.status == "200") {
-                callback(rawFile.responseText);
-            }
-        }
-        rawFile.send(null);
-    }
-    
-    //usage:
-    readTextFile("locations.json", function(text){
-      console.log("stage3")
-        var data = JSON.parse(text);
-        console.log(data);
-        quary = data;
-        myLoop(quary)
-    });
+$(document).ready(function(){
+  $.ajaxSetup({ cache: false });
+  $('#search').keyup(function(){
+   $('#result').html('');
+   $('#state').val('');
+   var searchField = $('#search').val();
+   var expression = new RegExp(searchField, "i");
+   $.getJSON('locations.json', function(data) {
+    $.each(data, function(key, value){
+     if (value.location_name.search(expression) != -1 || value.Zones__name.search(expression) != -1)
+     {
+      $('#result').append('<li class="list-group-item link-class"> '+value.location_name+' | <span class="text-muted">'+value.Zones__name+'</span></li>');
+     }
+    });   
+   });
+  });
   
-  function myLoop(x) { 
-    console.log("stage4")
-
-        var element = document.getElementById("demo");
-
-        var i, y, xLen;
-        xLen = Object.keys(quary).length;
-        for (i = 0; i < xLen; i++) {
-          console.log("stage5")
-          console.log(quary[i].location_name+"("+quary[i].Zones__name+")")
-
-          var newa = document.createElement("a");
-          var locationname = quary[i].location_name+"("+quary[i].Zones__name+")";
-          newa.innerHTML = locationname;
-          newa.setAttribute("href",quary[i].id)
-          document.getElementById("myDropdown").appendChild(newa)
-        }
-  };
-};
+  $('#result').on('click', 'li', function() {
+      location_nametxt = $(this).text().split('|')[0]
+      Zones__nametxt = $(this).text().split('|')[1]
+   var click_text = $(this).text().split('|');
+   $('#search').val($.trim(click_text[0]));
+   $("#result").html('');
+   $("#locationName").html(location_nametxt);
+   $("#zoneName").html(Zones_nametxt);
+  });
+ });
