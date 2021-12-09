48be5ad4c07e56ce23fc9d6184d66f14f9d4cc8f
DFARJUN <49616584+DFARJUN@users.noreply.github.com>
2021-12-09 20:48:25 +0200

    d

--- byzone/js.js
+++ byzone/js.js
@@ -29,7 +29,7 @@ $(document).ready(function(){
          $.each(data, function(key, value){
           if (value.Zones__name.search(expression) != -1)
           {
-           $('#locationdivul').append('<li class="list-group-item link-class"><input type="checkbox" id="cb'+value.id+'>'+value.location_name+'</li>');
+           $('#locationdivul').append('<li class="list-group-item link-class"><input type="checkbox" id="cb'+value.id+'">'+value.location_name+'</li>');
           }
          });   
         });      
